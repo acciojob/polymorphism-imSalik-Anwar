32c4e63832ad49e378580674355c982702a4e8f2
Salik Anwar <infernosight@gmail.com>
2023-08-09 10:33:49 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+        public void product(int x, int y){
 
+        }
+        public void product(int x, int y, int z){
+
+        }
+        public void product(double x, double y) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(2, 5);
+        p.product(2, 5, 8);
+        p.product(2.5, 5.6);
+
+    }
 }
